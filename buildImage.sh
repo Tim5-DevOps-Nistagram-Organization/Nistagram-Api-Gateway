c7acc0372f7c5b89382656d6a3ed09f2a74f44c2
Dusan Bucan <dusanbzr@gmail.com>
2021-07-08 09:49:15 +0200

    fix1

--- buildImage.sh
+++ buildImage.sh
@@ -8,6 +8,9 @@ APP_NAME=nistagramapigateway
 
 APP_IMAGE_NAME=${DOCKERHUB_USERNAME}/${APP_NAME}:${VERSION}
 
+echo $APP_IMAGE_NAME
+
+
 DOCKER_BUILDKIT=1 docker build \
 -t "${APP_IMAGE_NAME}" \
 --target gatewayRuntimeDev \ 
